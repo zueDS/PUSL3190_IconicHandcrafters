4a19c9cee41f0d6f01584c8981f5bf56cf6e09be
ShuttlerBad <dileepasudara@gmail.com>
2024-06-21 11:20:22 +0530

    Product Purchase guide page

---
+++ Purchaseguide.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+
+
+<head>
+    <meta charset="utf-8">
+    <title>About purchase</title>
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta property="og:title" content="">
+    <meta property="og:type" content="">
+    <meta property="og:url" content="">
+    <meta property="og:image" content="">
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="assets/imgs/theme/favicon.svg">
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="assets/css/main.css">
+</head>
+
+<body>
+     <?php
+        include './headerandmobileheader.php';
+        ?>
+    <main class="main">
+        <div class="page-header breadcrumb-wrap">
+            <div class="container">
+                <div class="breadcrumb">
+                    <a href="index.php" rel="nofollow">Home</a>
+                    
+                    <span></span> Purchase Guide
+                </div>
+            </div>
+        </div>
+        <section class="mt-50 mb-50">
+            <div class="container custom">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="single-page pr-30 mb-lg-0 mb-sm-5">
+                            <div class="single-header  style-2">
+                                <h2>Purchase Guide</h2>
+                                
+                            </div>
+                            <figure class="single-thumbnail">
+                                <img src="assets/imgs/page/img-1.jpg" alt="">
+                            </figure>
+                            <div class="single-content">
+                                <h4>1. Account Registering</h4>
+                                <p>You can registered as seller or buyer</p>
+                                
+                                <h4>2. Seller details</h4>
+                                <p>You can register as seller and can sell your products through Iconic Handcrafters.lk</p>
+                                <p>You will receive point as a seller for each activities.</p>
+                                <h4>3.Seller points</h4>
+                                <p>You will receive 10 points when you register.</p>
+                                <p>If user will purchase your products you will receive points.</p>
+                                <p>If user will reviewed your products you will receive points.</p>
+                                <h4>4.Seller levels</h4>
+                                <p>There are three seller levels.</p>
+                                <p>In level 2 you can add offers and discounts to your products.</p>
+                                <p>In level 3 you can offer coupons for your customers.</p>
+                                <p>In level 3 you can boost your products as a top rated seller.</p>
+                                <h4>5. As a buyer</h4>
+                                <p>You can purchase items.</p>
+                                <p>You can pay to the orders when the items are delivered(Cash on delivery service).</p>
+                                <span>*Delivery chargers may apply.</span>
+                                <p>You can offer gift cards to friends on site.</p>
+                                <p>You can review products.</p>
+                                
+                                <h4 class="mt-30">6. Delivery service</h4>
+                                <ul>
+                                    <li>We (Iconic Handcrafters) offer a high quality delivery service.</li>
+                                    <li>It will take the products from sellers and bring them to customers as fast as they can.</li>
+                                    <li>Buyers can check order location after we(Iconic Handcrafters) takeover package by seller.</li>
+                                    
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                   
+                </div>
+            </div>
+        </section>
+    </main>
+    <?php
+        include './Footer.php';
+        ?>
+    <!-- Preloader Start -->
+    <div id="preloader-active">
+        <div class="preloader d-flex align-items-center justify-content-center">
+            <div class="preloader-inner position-relative">
+                <div class="text-center">
+                    <h5 class="mb-5">Now Loading</h5>
+                    <div class="loader">
+                        <div class="bar bar1"></div>
+                        <div class="bar bar2"></div>
+                        <div class="bar bar3"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Vendor JS-->
+    <script src="assets/js/vendor/modernizr-3.6.0.min.js"></script>
+    <script src="assets/js/vendor/jquery-3.6.0.min.js"></script>
+    <script src="assets/js/vendor/jquery-migrate-3.3.0.min.js"></script>
+    <script src="assets/js/vendor/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/plugins/slick.js"></script>
+    <script src="assets/js/plugins/jquery.syotimer.min.js"></script>
+    <script src="assets/js/plugins/wow.js"></script>
+    <script src="assets/js/plugins/jquery-ui.js"></script>
+    <script src="assets/js/plugins/perfect-scrollbar.js"></script>
+    <script src="assets/js/plugins/magnific-popup.js"></script>
+    <script src="assets/js/plugins/select2.min.js"></script>
+    <script src="assets/js/plugins/waypoints.js"></script>
+    <script src="assets/js/plugins/counterup.js"></script>
+    <script src="assets/js/plugins/jquery.countdown.min.js"></script>
+    <script src="assets/js/plugins/images-loaded.js"></script>
+    <script src="assets/js/plugins/isotope.js"></script>
+    <script src="assets/js/plugins/scrollup.js"></script>
+    <script src="assets/js/plugins/jquery.vticker-min.js"></script>
+    <script src="assets/js/plugins/jquery.theia.sticky.js"></script>
+    <!-- Template  JS -->
+    <script src="assets/js/main.js"></script>
+</body>
+
+
+</html>
