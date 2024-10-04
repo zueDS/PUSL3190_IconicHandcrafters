9913bed2b3c5e6f4607fb5db3a6915f643260992
ShuttlerBad <dileepasudara@gmail.com>
2024-10-04 12:21:36 +0530

    seller mark item out of stock

---
+++ Phpfiles/sellermarkitemasnotqty.php
@@ -0,0 +1,17 @@
+<?php
+
+include './DB.php';
+
+
+
+$row_id = $_POST["userid"];
+$sellerstat = "Out of stock";
+
+$sql = "update sellerpostitem set stockavailab='$sellerstat' where itemidsellerpost='$row_id'";
+if ($conn->query($sql) === TRUE) {
+
+    echo "ok";
+} else {
+echo "Error: " . $sql . "<br>" . $conn->error;
+
+}
